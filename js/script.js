819c70e36911bd6830348b1bda0998e025037285
sebas2426 <tobiaspalma505@gmail.com>
2025-06-21 21:26:32 -0400

    Mejoras en el modo oscuro

--- js/script.js
+++ js/script.js
@@ -4,10 +4,38 @@ const hamMenuOff= document.querySelector('.ham-menu-off');
 
 const irTopBtn= document.querySelector('.ir-inicio');
 
-const btnModoOscuro= document.getElementById('btn-modo-oscuro');
-
 const elementos= document.getElementById('modo');
 
+const iconoModoOscuro= document.getElementById('icono-modo-oscuro');
+
+let modoOscuro= localStorage.getItem('modo-oscuro');
+
+if(modoOscuro==='activo'){
+        document.body.classList.add('modo-oscuro');
+        localStorage.setItem('modo-oscuro', 'activo');
+}
+
+function aplicarModoOscuro(){
+    document.body.classList.add('modo-oscuro');
+        localStorage.setItem('modo-oscuro', 'activo');
+}
+
+function desactivarModoOscuro(){
+    document.body.classList.remove('modo-oscuro');
+        localStorage.setItem('modo-oscuro', null);
+}
+
+iconoModoOscuro.addEventListener('click', ()=>{
+    modoOscuro=localStorage.getItem('modo-oscuro');
+
+    iconoModoOscuro.classList.toggle('bi-moon');
+
+    if(modoOscuro!=='activo'){
+        aplicarModoOscuro();
+    }else{
+        desactivarModoOscuro();
+    }
+});
 
 
 
@@ -54,7 +82,3 @@ productContainers.forEach((item, i) => {
     });
 });
 
-
-btnModoOscuro.addEventListener('click', ()=>{
-    document.body.classList.toggle('modo-oscuro');
-});
